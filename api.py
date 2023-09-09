a75da36c88c2f30751074f962fd0156ad86f809c
Ethan Gutierrez <Ethan.e.gutierrez@gmail.com>
2023-09-08 17:41:49 -0700

    removing app run for testing

--- api.py
+++ api.py
@@ -11,5 +11,3 @@ app.register_blueprint(posts_bp)
 app.register_blueprint(comments_bp)
 app.register_blueprint(auth_bp)
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int("5001"), debug=True)
